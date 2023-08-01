c3db67dcabe64547592ff90ea9d3509d44263a30
Aditya Mane <maneaditya455@gmail.com>
2023-08-01 11:07:44 +0530

    Added delete button

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const buttons = document.querySelectorAll('button')
+const display = document.getElementById("result")
+const bigContainer = document.getElementById("container-id")
+
+buttons.forEach((a) => {
+    a.addEventListener("click", () => {
+        if(a.textContent == "DEL"){
+            display.value = display.value.slice(0, -1)
+        }
+        else if(a.textContent == "AC"){
+            display.value = ''
+        }
+        else if(a.textContent == '='){
+            display.value = eval(display.value)
+
+        }
+        else{
+            display.value += a.textContent
+        }
+    })
+})
