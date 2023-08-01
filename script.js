ff468895e15b125c1cbc8395a5db42c6ce26f3eb
Aditya Mane <maneaditya455@gmail.com>
2023-08-01 11:55:32 +0530

    checking correct file

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 const buttons = document.querySelectorAll('button')
 const display = document.getElementById("result")
 const bigContainer = document.getElementById("container-id")
@@ -20,31 +18,4 @@ buttons.forEach((a) => {
             display.value += a.textContent
         }
     })
-=======
-
-const buttons = document.querySelectorAll('button')
-const display = document.getElementById("result")
-const bigContainer = document.getElementById("container-id")
-
-buttons.forEach((a) => {
-    a.addEventListener("click", () => {
-        if(a.textContent == "C"){
-            display.value = ''
-        }
-        else if(a.textContent == '='){
-            display.value = eval(display.value)
-
-        }
-        else if(a.textContent == 'sq'){
-            display.value = Math.sqrt(display.value)
-        }
-        else if(a.textContent == '^2'){
-            display.value += a.textContent
-        }
-
-        else{
-            display.value += a.textContent
-        }
-    })
->>>>>>> 323740edd15ac0aa6cddfe00c72f1c2370c74d2a
 })
